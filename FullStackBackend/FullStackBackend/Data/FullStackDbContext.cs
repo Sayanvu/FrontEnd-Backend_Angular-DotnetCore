7baf0f32dccb45af779737ebe80e60ee0d413a67
Sayanvu <106749161+Sayanvu@users.noreply.github.com>
2023-06-14 09:54:40 +0530

    Add files via upload

---
+++ FullStackBackend/FullStackBackend/Data/FullStackDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FullStackBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackBackend.Data
+{
+    public class FullStackDbContext:DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
